9455ad98dfed15919953c68cdc8f1bf9444a5779
icnocop <icnocop@users.noreply.github.com>
2019-03-09 17:16:49 -0800

    Using powershell to copy files

---
+++ CopyFiles.ps1
@@ -0,0 +1,9 @@
+﻿# Enable -Verbose option
+[CmdletBinding()]
+
+# Set a flag to force verbose as a default
+$VerbosePreference ='Continue' # equiv to -verbose
+
+Copy-Item -Path "$env:ChocolateyInstall\log" -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY" –Recurse
+Copy-Item -Path "$env:TEMP\chocolatey" -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY" –Recurse
+Copy-Item -Path "$env:ChocolateyInstall\config" -Destination "$env:BUILD_ARTIFACTSTAGINGDIRECTORY" –Recurse
